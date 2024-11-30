0d563d6e7d954d7c823179474c8b3241aab44d2d
zfq <zhangfengqianbupt@bupt.edu.cn>
2024-11-30 18:05:05 +0800

    feat(DNSName): 重构并添加新功能
    - 根绝参考项目的测试类补充了一些方法以及测试函数
    - 新增了 `offsets` 字段, 用来记录每个 label 在 name 中的偏移量:
      - bing9 实现中就有这个字段
      - 添加了这个字段后许多功能实现起来比较方便 (之前的方法也进行了重构)
    - 解决了面对转义字符时解析时出现的错误 (测试用例就是牛啊)
    - 添加新功能:
      - 新的构造函数 (从字符串构造, 空构造)
      - GetRawLabels: 获取 DNSName 的所有标签
      - Include/IsInclude: 检查一个 DNSName 是否包含另一个
      - MakeRelative: 将 DNSName 相对于另一个 DNSName
      - LabelReverse: 反转 DNSName 的标签顺序
      - PrependRawLabel/AppendRawLabel: 在 DNSName 前后添加标签
      - CountLabels: 统计 DNSName 的标签数量
      - 实现 DNSName 之间的比较运算符
    - 添加 (抄写) 单元测试,提高代码覆盖率

--- CSDNS.Tests/DNSNameTest.cs
+++ CSDNS.Tests/DNSNameTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,11 +10,154 @@ namespace CSDNS.Tests;
 public class DNSNameTest
 {
     [TestMethod]
-    public void METHOD()
+    public void NewMethodTest()
     {
+        // TODO: 我想这个测试函数是不是没有什么意义? 因为下面很多函数在测试时都使用 new method
         var dnsName = new DNSName("www.bupt.edu.cn"u8.ToArray());
-        Console.WriteLine(dnsName.ToString());
-        dnsName = new DNSName("www.bupt.edu.cn."u8.ToArray());
-        Console.WriteLine(dnsName.ToString(trailing: false));
+        Assert.AreEqual(dnsName.ToString(), "www.bupt.edu.cn.");
+        var dnsName2 = new DNSName("www.bupt.edu.cn.");
+        Assert.AreEqual(dnsName2.ToString(trailing: false), "www.bupt.edu.cn");
+        // 超长字符串测试
+        var jpmens =
+            "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc." +
+            "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb." +
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa." +
+            "test.xxx.yyy-yyyy.zzzzzzzzz-test.";
+        var jpmensName = new DNSName(jpmens);
+        Assert.AreEqual(jpmensName.ToString(), jpmens);
+        Assert.ThrowsException<FormatException>(() => new DNSName("bert..Hubert."));
+    }
+
+    [TestMethod]
+    public void OperatorTest()
+    {
+        var wwwds9anl = new DNSName("www.ds9a.nl.");
+        Assert.IsTrue(wwwds9anl == wwwds9anl); // 指向相同对象
+        Assert.IsTrue(new DNSName("wWw.ds9A.Nl.") == new DNSName("www.ds9a.nl.")); // 大小写不同
+        Assert.IsTrue(new DNSName("www.ds9a.nl.") == new DNSName("www.ds9a.nl.")); // 创建两个对象然后比较 (创建方式相同, 但指向不同)
+
+        DNSName name = new("."), parent = new();
+        Assert.IsTrue(name != parent);
+        
+        // TODO: 这里有两个比较函数没有实现 (因为看不懂具体含义), `<` 以及 `canonCompare`
+    }
+
+    [TestMethod]
+    public void IsIncludeTest()
+    {
+        DNSName wwwds9anl = new("www.ds9a.nl."), wwwds9anl1 = new("www.ds9a\002nl."), nl = new("nl.");
+        Assert.IsTrue(wwwds9anl.IsInclude(nl));
+        Assert.IsTrue(!wwwds9anl1.IsInclude(nl));
+        Assert.IsTrue(wwwds9anl.IsInclude(wwwds9anl));
+        Assert.IsTrue(!nl.IsInclude(wwwds9anl));
+
+        // Check name part of root
+        {
+            DNSName name = new("a."), parent = new(".");
+            Assert.IsTrue(name.IsInclude(parent));
+        }
+        // Label boundary
+        {
+            DNSName name = new("a\002bb."), parent = new("bb.");
+            Assert.IsTrue(!name.IsInclude(parent));
+        }
+        // Multi label parent
+        {
+            DNSName name = new("a.bb.ccc.dddd."), parent = new("ccc.dddd.");
+            Assert.IsTrue(name.IsInclude(parent));
+        }
+        // Last char diff (测试等号之类的, 还真测试出来一个)
+        {
+            DNSName name = new("a.bb.ccc.dddd."), parent = new("ccc.dddx.");
+            Assert.IsTrue(!name.IsInclude(parent));
+        }
+        // Equal length identical
+        {
+            DNSName name = new("aaaa.bbb.cc.d."), parent = new("aaaa.bbb.cc.d.");
+            Assert.IsTrue(name.IsInclude(parent));
+        }
+        // Equal length first char diff
+        {
+            DNSName name = new("xaaa.bbb.cc.d."), parent = new("aaaa.bbb.cc.d.");
+            Assert.IsTrue(!name.IsInclude(parent));
+        }
+    }
+
+    /// <summary>
+    /// 一些比较简单的函数的测试
+    /// </summary>
+    [TestMethod]
+    public void LittleFunctionTest()
+    {
+        // GetRawLabelsTest
+        {
+            var before = "www.ds9a.nl.";
+            var b = new DNSName(before);
+            Assert.AreEqual(b.GetRawLabels().Count, 3);
+            var after = b.ToString();
+            Assert.AreEqual(before, after);
+        }
+        // LabelReverseTest
+        {
+            var name = new DNSName("aaaa.bbb.cc.d.");
+            Assert.IsTrue(name.LabelReverse() == new DNSName("d.cc.bbb.aaaa.")); // 注意这里使用 `==` 进行比较而不是 `AreEquals`
+        }
+        // MakeRelativeTest
+        {
+            DNSName name = new("aaaa.bbb.cc.d."), parent = new("cc.d.");
+            Assert.IsTrue(name.MakeRelative(parent) == new DNSName("aaaa.bbb."));
+        }
+        // EmptyTest
+        {
+            var name = new DNSName();
+            Assert.IsTrue(name.Empty());
+        }
+        // RootTest
+        {
+            DNSName name = new("."), rootnodot = new("");
+            // 这个测试代码有问题, 在参考项目中 emoty 是直接被声明成 `string empty`, 可能和 C++ 和 C# 语言特性有关
+            // String empty; Assert.IsTrue(new DNSName(empty) == name);
+            Assert.IsTrue(!name.Empty());
+            Assert.IsTrue(name == rootnodot);
+        }
+    }
+
+    [TestMethod]
+    public void PrependAndAppendRawLabelTest()
+    {
+        // TODO: 对 `Prepend` 的测试有点少
+        var left = new DNSName("ds9a.nl.");
+        left.PrependRawLabel("www");
+        Assert.IsTrue(left == new DNSName("WwW.dS9A.Nl."));
+        left.AppendRawLabel("com");
+        Assert.IsTrue(left == new DNSName("WwW.Ds9A.Nl.com."));
+
+        var unset = new DNSName();
+        unset.AppendRawLabel("www").AppendRawLabel("powerdns.com").AppendRawLabel("com");
+        Assert.IsTrue(unset.ToString() == "www.powerdns\\.com.com.");
+
+        var build = new DNSName();
+        build.AppendRawLabel("Donald E. Eastlake 3rd");
+        build.AppendRawLabel("example");
+        Assert.IsTrue(build.ToString() == @"Donald\032E\.\032Eastlake\0323rd.example.");
+    }
+    
+    /// <summary>
+    /// 最重要的测试, 设计如何处理各种转义字符的问题
+    /// </summary>
+    [TestMethod]
+    public void RFC4343Test()
+    {
+        // 2024:11/30 17:16 通过写单元测试还真检查出一些错误
+        DNSName rfc4343_2_1 = new("~!.example."),
+            rfc4343_2_2 = new(@"Donald\032E\.\032Eastlake\0323rd.example."),
+            example = new("example.");
+        Assert.IsTrue(rfc4343_2_1.IsInclude(example));
+        Assert.IsTrue(rfc4343_2_2.IsInclude(example));
+        Assert.IsTrue(rfc4343_2_1.ToString() == "~!.example.");
+        var labels = rfc4343_2_2.GetRawLabels();
+        Assert.IsTrue(labels[0].SequenceEqual("Donald E. Eastlake 3rd"u8.ToArray()));
+        Assert.IsTrue(labels[^1].SequenceEqual("example"u8.ToArray()));
+        Assert.IsTrue(labels.Count == 2);
     }
 }
--- CSDNS/DNSName.cs
+++ CSDNS/DNSName.cs
@@ -2,20 +2,29 @@
 
 namespace CSDNS;
 
+internal enum State
+{
+    LabelBegin, // 域名中一个标签开始
+    LabelIn, // 在一个标签处理过程中
+    LabelEnd, //域名中一个标签结束
+
+    // 解析转义字符后面的数字, 一定解析后面的三个字符, 如果不到三个字符则抛出异常, 到了三个字符就进入到上面的三个状
+    BeginEscapeNumber,
+    InEscapeNumber, // 进入对转移字符转义的状态
+    EndEscapeNumber, // 对数字进行转义
+}
+
+internal class ByteComparer : IEqualityComparer<Byte>
+{
+    private static Byte ToLower(Byte b) => (Byte)Char.ToLower((Char)b);
+
+    public bool Equals(Byte x, Byte y) => ToLower(x) == ToLower(y);
+
+    public int GetHashCode(Byte obj) => ToLower(obj).GetHashCode();
+}
+
 public class DNSName
 {
-    private enum State
-    {
-        LabelBegin, // 域名中一个标签开始
-        LabelIn, // 在一个标签处理过程中
-        LabelEnd, //域名中一个标签结束
-
-        // 解析转义字符后面的数字, 一定解析后面的三个字符, 如果不到三个字符则抛出异常, 到了三个字符就进入到上面的三个状
-        BeginEscapeNumber,
-        InEscapeNumber, // 进入对转移字符转义的状态
-        EndEscapeNumber, // 对数字进行转义
-    }
-
     /// <summary>
     ///  定义一些程序用到的常量:
     ///   * `MaxDNSNameLength` 表示一整个 DNSName 的长度不应该超过 255 个字节;
@@ -30,20 +39,38 @@ public class DNSName
         BackslashByte = (Byte)'\\';
 
     private Byte[] name;
+    private Int32[] offsets; // 这个变量参考项目中没有我为了实现简单自己定义了一个
+
+
+    public DNSName()
+    {
+        name = [];
+        offsets = [];
+    }
+
+    public DNSName(DNSName rhs)
+    {
+        name = rhs.name;
+        offsets = rhs.offsets;
+    }
+
+    // 从一个字符串解析为 dns
+    public DNSName(String s) : this(Encoding.UTF8.GetBytes(s)) { }
 
     public DNSName(Byte[] bytes)
     {
         if (bytes.Length == 0 || bytes is [PointByte])
+        {
             name = [EndByte];
+            offsets = [0];
+        }
         else
-            name = ResolveDNSName(bytes);
+        {
+            var output = ResolveDNSName(bytes);
+            name = output.Item1;
+            offsets = output.Item2;
+        }
     }
-
-    private Boolean IsDigit(Byte b) => b is >= (Byte)'0' and <= (Byte)'9';
-
-    private Boolean IsRoot() => name[0] == EndByte;
-
-    private Boolean Empty() => name.Length == 0;
 
     /// <summary>
     /// 把 一个 Byte 数组中的 DNSName 转换为对人类友好的字符串
@@ -60,13 +87,11 @@ public class DNSName
         if (IsRoot())
             return trailing ? separator : "";
         var stringBuilder = new StringBuilder();
-        var i = 0;
-        while (i < name.Length && name[i] != EndByte)
+        foreach (var offset in offsets)
         {
-            var labelLength = name[i];
-            ToLabel(stringBuilder, i + 1, labelLength);
+            var labelLength = name[offset];
+            ToLabel(stringBuilder, offset + 1, labelLength);
             stringBuilder.Append(separator);
-            i += labelLength + 1;
         }
 
         if (!trailing)
@@ -74,6 +99,201 @@ public class DNSName
 
         return stringBuilder.ToString();
     }
+
+    /// <summary>
+    /// 获取一个 DNSName 的所有标签; 使用 Bute[] 进行存储, 而不是像原来项目一样使用字符串进行存储
+    /// </summary>
+    public List<Byte[]> GetRawLabels()
+    {
+        var ret = new List<Byte[]>(offsets.Length);
+        foreach (var offset in offsets)
+        {
+            var labelLength = name[offset];
+            if (labelLength == 0)
+                break;
+            var label = new Byte[labelLength];
+            Array.Copy(name, offset + 1, label, 0, labelLength);
+            ret.Add(label);
+        }
+
+        return ret;
+    }
+
+    public Boolean Include(DNSName other) => IsInclude(other);
+
+    /// <summary>
+    /// 判断当前域名是否含有另一个域名; 使用这个方法替换了参考项目里面的 `isPartOf` 因为我觉得这个写的不是很好
+    /// TODO: 感觉这个函数的命名不是很合适, 希望以后能改进
+    /// </summary>
+    /// <param name="other">另外一个域名</param>
+    /// <returns></returns>
+    public Boolean IsInclude(DNSName other)
+    {
+        // 这些 if 条件判断, 有的是用来加速判断, 有的是真有用
+        // 自己一定包含自己 
+        if (this == other) return true;
+        // 一定包含空和 Root (包含 root 的前提是自己非空)
+        if (other.Empty() || (!Empty() && other.IsRoot())) return true;
+        // 长的不可能包含短的 (包括 label 的个数)
+        if (name.Length < other.name.Length || offsets.Length < other.offsets.Length) return false;
+        for (var i = 1; i <= other.offsets.Length; i++)
+        {
+            // t 和 o 分别表示 this 和 other
+            Int32 tLabelLength = name[offsets[^i]], oLabelLength = other.name[other.offsets[^i]];
+            if (tLabelLength != oLabelLength) return false;
+            for (var j = 1; j <= oLabelLength; j++)
+                if (name[offsets[^i] + j] != other.name[other.offsets[^i] + j])
+                    return false;
+        }
+
+        return true;
+    }
+
+    public static Boolean operator ==(DNSName lhs, DNSName rhs)
+    {
+        // 如果两者指向同一个对象, 则说明两者相等
+        if (ReferenceEquals(lhs, rhs)) return true;
+        // 如果两个 DNSName 的 label 个数以及 name 的长度都不相等,那一定不相等
+        if (lhs.offsets.Length != rhs.offsets.Length || lhs.name.Length != rhs.name.Length) return false;
+        // 最后按顺序比较 name (这里默认如果 name 一致, 那么 offsets 也就一致了)
+        return lhs.name.SequenceEqual(rhs.name, new ByteComparer());
+    }
+
+    public static Boolean operator !=(DNSName lhs, DNSName rhs) => !(lhs == rhs);
+
+    public Boolean Empty() => name.Length == 0;
+
+    public DNSName MakeRelative(DNSName zone)
+    {
+        if (IsInclude(zone))
+        {
+            name = name.Take(name.Length - zone.name.Length).Append<Byte>(0).ToArray();
+            offsets = offsets.Take(offsets.Length - zone.offsets.Length).ToArray();
+        }
+        else
+            throw new NotImplementedException("暂未实现: 如果不包含在内的处理方式, 因为我觉得参考项目中的实现方式不是很合理");
+
+        return this;
+    }
+
+
+    // TODO: 这个函数的命名不是很合适, 应该是动词在前名词在后 (对函数来说如此)
+    public DNSName LabelReverse()
+    {
+        var ret = new DNSName();
+        if (IsRoot()) return this;
+        if (Empty()) return ret;
+        var labels = GetRawLabels();
+        labels.Reverse();
+        ret.AppendRawLabels(labels);
+        return ret;
+    }
+
+
+    // TODO: `PrependRawLabel(String label)` 和 `AppendRawLabel(String label)` 函数都一个很大的问题就是
+    //       不对字符串中存在的转义字符进行转义, 不知道这一点需不需要做
+
+    public DNSName PrependRawLabel(String label) => PrependRawLabel(Encoding.UTF8.GetBytes(label));
+
+    public DNSName PrependRawLabel(Byte[] label)
+    {
+        CheckLabelLength(label.Length);
+        if (name.Length + label.Length > MaxDNSNameLength - 1)
+            throw new IndexOutOfRangeException(
+                $"新增的 dns name 长度 {name.Length + label.Length} 大于 DNSName 最大长度 {MaxDNSNameLength}");
+        var nameList = name.ToList();
+        var offsetsList = offsets.ToList();
+        if (Empty())
+        {
+            nameList.Add((Byte)label.Length);
+            nameList.AddRange(label);
+            nameList.Add(0);
+            name = nameList.ToArray();
+            offsets = [0];
+        }
+        else
+        {
+            nameList.Insert(0, (Byte)label.Length);
+            nameList.InsertRange(1, label);
+            var addLength = label.Length + 1;
+            for (var i = 1; i < offsetsList.Count; i++)
+                offsetsList[i] += addLength;
+            offsetsList.Insert(1, addLength);
+            name = nameList.ToArray();
+            offsets = offsetsList.ToArray();
+        }
+
+        return this;
+    }
+
+    private static void CheckLabelLength(Int32 length)
+    {
+        switch (length)
+        {
+            case 0:
+                throw new IndexOutOfRangeException("no such thing as an empty label to append");
+            case > MaxLabelLength:
+                throw new IndexOutOfRangeException($"label length {length} exceeds maximum of {MaxLabelLength}");
+        }
+    }
+
+    /// <summary>
+    /// 针对追加多个标签的情景进行了专属的优化, 省去了不少检查以及 List 和 Array 转化所造成的性能损失 (没测试过字节感觉性能损失不小)
+    /// </summary>
+    /// <param name="labels"></param>
+    /// <exception cref="IndexOutOfRangeException"></exception>
+    private void AppendRawLabels(List<Byte[]> labels)
+    {
+        var nameList = name.ToList();
+        var offsetsList = offsets.ToList();
+        if (!Empty()) nameList.RemoveAt(nameList.Count - 1); // 如果不为空记得删除末尾的 0 
+        foreach (var label in labels)
+        {
+            CheckLabelLength(label.Length);
+            if (nameList.Count + label.Length > MaxDNSNameLength - 1)
+                throw new IndexOutOfRangeException(
+                    $"新增的 dns name 长度 {nameList.Count + label.Length} 大于 DNSName 最大长度 {MaxDNSNameLength}");
+            offsetsList.Add(nameList.Count);
+            nameList.Add((Byte)label.Length);
+            nameList.AddRange(label);
+        }
+
+        nameList.Add(0);
+        name = nameList.ToArray();
+        offsets = offsetsList.ToArray();
+    }
+
+    public DNSName AppendRawLabel(String label) => AppendRawLabel(Encoding.UTF8.GetBytes(label));
+
+    public DNSName AppendRawLabel(Byte[] label)
+    {
+        CheckLabelLength(label.Length);
+
+        if (name.Length + label.Length > MaxDNSNameLength - 1)
+            throw new IndexOutOfRangeException(
+                $"新增的 dns name 长度 {name.Length + label.Length} 大于 DNSName 最大长度 {MaxDNSNameLength}");
+        // 这里使用 List 来对 name 和 offsets 进行拼接操作 (也许不是最高效的方式?)
+        // 参考项目中有根据 label 的长度预先分配空间的代码, 但是 C# 并没有这么精细级别的操作, 所以没有把那部分代码抄过来
+        // 考虑到我是使用数组进行存储的, 这里我们假定数组是不可变的, 所以这里使用 List 进行拼接
+        var nameList = name.ToList();
+        var offsetsList = offsets.ToList();
+        if (!Empty()) nameList.RemoveAt(nameList.Count - 1);
+        offsetsList.Add(nameList.Count);
+        nameList.Add((Byte)label.Length);
+        nameList.AddRange(label);
+        nameList.Add(0);
+        name = nameList.ToArray();
+        offsets = offsetsList.ToArray();
+        return this;
+    }
+    
+    public Int32 GetLabelCount() => offsets.Length;
+
+    public Int32 CountLabels() => offsets.Length; // TODO: 这个函数纯纯是为了和参考项目中函数名对应才写的, 以后有机会删除它
+    
+    private Boolean IsDigit(Byte b) => b is >= (Byte)'0' and <= (Byte)'9';
+
+    private Boolean IsRoot() => name[0] == EndByte;
 
     /// <summary>
     ///  将一个 Byte 数组中的 DNSName 标签转换为字符串. 等价于参考项目中的 `appendEscapedLabel` 函数
@@ -107,17 +327,20 @@ public class DNSName
         }
     }
 
-
     /// <summary>
     ///  解析 DNSName, 并且返回一个 Byte 数组, 这个数组中包含一个 DNSName 的所有字节, 包括尾随的零
     /// </summary>
     /// <param name="bytes"></param>
     /// <returns>解析后的 DNSName</returns>
-    /// <example>www.bupt.edu.cn -> 3www4bupt3edu2cn0</example>
+    /// <example>
+    /// www.bupt.edu.cn -> 3www4bupt3edu2cn0
+    /// offsets -> [0, 4, 9, 13]
+    /// </example>
     /// <exception cref="FormatException"></exception>
-    private Byte[] ResolveDNSName(Byte[] bytes)
+    private Tuple<Byte[], Int32[]> ResolveDNSName(Byte[] bytes)
     {
-        List<Byte> label = new(MaxLabelLength), ret = new(MaxDNSNameLength);
+        List<Byte> label = new(MaxLabelLength), retName = new(MaxDNSNameLength);
+        var retOffsets = new List<Int32>();
         var value = 0;
         var state = State.LabelBegin;
         foreach (var b in bytes)
@@ -131,16 +354,18 @@ public class DNSName
                     goto case State.LabelIn;
                 case State.LabelIn:
                     if (b == BackslashByte)
-                        goto case State.BeginEscapeNumber;
-                    if (b == PointByte)
+                        state = State.BeginEscapeNumber;
+                    else if (b == PointByte)
                         goto case State.LabelEnd;
-                    label.Add(b);
+                    else
+                        label.Add(b);
                     break;
                 case State.LabelEnd:
                     if (label.Count > MaxLabelLength)
                         throw new FormatException("DNSName 中存在标签长度超过 63 个字节的情况");
-                    ret.Add((Byte)label.Count);
-                    ret.AddRange(label);
+                    retOffsets.Add(retName.Count);
+                    retName.Add((Byte)label.Count);
+                    retName.AddRange(label);
                     label.Clear(); // 识别完一个标签记得清空一下
                     state = State.LabelBegin;
                     break;
@@ -185,12 +410,13 @@ public class DNSName
         {
             if (label.Count > MaxLabelLength)
                 throw new FormatException("DNSName 中存在标签长度超过 63 个字节的情况");
-            ret.Add((Byte)label.Count);
-            ret.AddRange(label);
+            retOffsets.Add(retName.Count);
+            retName.Add((Byte)label.Count);
+            retName.AddRange(label);
         }
 
-        ret.Add(0); // 添加尾随零
+        retName.Add(0); // 添加尾随零
 
-        return ret.ToArray();
+        return new Tuple<Byte[], Int32[]>(retName.ToArray(), retOffsets.ToArray());
     }
 }
